487173ab95c0fdd190ac941eba20b27d8cff756f
avavion <avavionmvm@gmail.com>
2020-09-12 01:37:51 +0300

    Refresh README, added new style on components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 
+import TodoItem from "./components/TodoItem/TodoItem";
+
 function App() {
   return (
-    <div>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
+    <div className="todo-list">
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
+      <TodoItem></TodoItem>
     </div>
   );
 }
